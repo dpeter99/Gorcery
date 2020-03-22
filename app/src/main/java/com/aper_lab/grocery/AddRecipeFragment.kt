8b6babbd93815ffe76185b8f9ec376bb40370aab
dpeter99 <dpeter99@gmail.com>
2020-03-22 18:12:57 +0100

    [Sync]

--- app/src/main/java/com/aper_lab/grocery/AddRecipeFragment.kt
+++ app/src/main/java/com/aper_lab/grocery/AddRecipeFragment.kt
@@ -1,6 +1,7 @@
 package com.aper_lab.grocery
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -32,6 +33,7 @@ class AddRecipeFragment : Fragment() {
 
         binding.btnImport.setOnClickListener {
             viewModel.importRecipe(binding.recipeUrl.text.toString());
+            Log.e("Test", viewModel.successfulImport.value.toString())
         }
 
 
